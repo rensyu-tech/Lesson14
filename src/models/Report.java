acf7b85322550ee877af954c1d743d0614409860
rensyu.tech <practicetechacademy899@gmail.com>
2019-07-04 22:56:10 +0900

    Add Report edit

--- src/models/Report.java
+++ src/models/Report.java
@@ -25,6 +25,14 @@ import javax.persistence.Table;
             name = "getReportsCount",
             query = "SELECT COUNT(r) FROM Report AS r"
             ),
+    @NamedQuery(
+            name = "getMyAllReports",
+            query = "SELECT r FROM Report AS r WHERE r.employee = :employee ORDER BY r.id DESC"
+            ),
+    @NamedQuery(
+            name = "getMyReportsCount",
+            query = "SELECT COUNT(r) FROM Report AS r WHERE r.employee = :employee"
+            )
 })
 @Entity
 public class Report {
@@ -52,6 +60,8 @@ public class Report {
 
     @Column(name = "updated_at", nullable = false)
     private Timestamp updated_at;
+
+
 
     public Integer getId() {
         return id;
